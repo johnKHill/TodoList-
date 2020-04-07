31f0d9ededed7d1d67bc74995e9d9b64f9c1b418
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-04-07 13:28:07 -0400

    Wrote code to fetch all my todos from my JSON api and added them to my app home page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-// alert("HI FROM APP JS!")
+$(document).ready(function() {
+  // Making a request to my own api
+  $.getJSON("/api/todos")
+  .then(addTodos)
+});
+
+function addTodos(todos) {
+  // add todos to the page
+  todos.forEach(function(todo) {
+    let newTodo = $("<li>"+ todo.name + "</li>");
+    newTodo.addClass("task");
+
+    $(".list").append(newTodo);
+  })
+
+}
