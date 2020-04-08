63d67ebca053f30318034fd65c0a373abb13b449
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-04-08 11:40:12 -0400

    Added the delete btn and a remove method using Jquery's .ajax method. I used Jquery's .data method to store meta-data for deletion

--- public/app.js
+++ public/app.js
@@ -2,15 +2,64 @@ $(document).ready(function() {
   // Making a request to my own api
   $.getJSON("/api/todos")
   .then(addTodos)
+
+  // Adding event listeners when the page loads
+  $("#todoInput").keypress(function(event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
+
+  $(".list").on("click", "span", function() {
+    removeTodo($(this).parent());
+  });
 });
 
 function addTodos(todos) {
   // add todos to the page
   todos.forEach(function(todo) {
-    let newTodo = $("<li>"+ todo.name + "</li>");
-    newTodo.addClass("task");
+    addTodo(todo);
+  });
+}
+function addTodo(todo) {
+  let newTodo = $("<li class='task'>" + todo.name + " <span>X</span></li>");
+  // Mongo is storing the _id for the todos. I'm storing it using Jquery
+  // mete-data '.data()' to store the _id also
+  newTodo.data('id', todo._id);
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $(".list").append(newTodo);
+}
 
-    $(".list").append(newTodo);
+
+
+
+function createTodo() {
+  // send request to create new todo
+  let usrInput = $("#todoInput").val();
+  $.post("/api/todos", {name: usrInput})
+  .then(function(newTodo) {
+    $("#todoInput").val("");
+    addTodo(newTodo);
+  })
+  .catch(function(err) {
+    console.log(err);
   })
 
+}
+
+function removeTodo(todo) {
+  let clickedId = todo.data("id");
+  let deleteUrl = "/api/todos/" + clickedId;
+  $.ajax({
+    method: "DELETE",
+    url: deleteUrl
+  })
+  .then(function(data) {
+    todo.remove();
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 }
